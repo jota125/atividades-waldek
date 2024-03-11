6c1cabc0b1ee0143e4b45c6cf1ded0b9b63f3d8b
jonathan vinicius <90495222+jota125@users.noreply.github.com>
2024-03-11 17:57:47 -0300

    Create lista2.js

---
+++ lista-slide-7/lista2.js
@@ -0,0 +1,50 @@
+var readline = require("readline-sync");
+
+//1 
+var numero = 10
+console.log (numero)
+
+//2
+var numero = 10;
+numero += 5;
+console.log (numero)
+// '=' é usado para atribuir um valor a uma variável.
+// '+=' é usado para adicionar um valor a uma variável e atribuir o resultado de volta à variável.
+
+//3
+
+var numero = 10;
+numero -= 4
+console.log(numero)
+//operador -= é uma forma concisa de subtrair um valor de uma variável e atribuir o resultado de volta à mesma variável em uma única operação.
+
+//4
+var contador = 0;
+contador++;
+//O operador ++ aumenta o valor da variável em 1.
+
+//5
+var x = 10;
+x *= 5; // Equivalente a x = x * 5;
+
+//6
+var x = 10;
+var y = x;
+//o valor de x (que é 10) é atribuído à variável y. Depois disso, ambas x e y terão o valor 10.
+
+//7
+//*= é uma forma concisa de multiplicar o valor de uma variável por outro número e atribuir o resultado de volta à mesma variável em uma única operação.
+
+//8
+var x = 10;
+x /= 2; // Equivalente a x = x / 2;
+
+//9
+var string1 = "Olá";
+var string2 = " mundo";
+var resultado = string1 + string2;
+console.log(resultado); // Saída: "Olá mundo"
+
+//10
+//o operador /= é uma maneira concisa de dividir o valor de uma variável por outro número e atribuir o resultado de volta à mesma variável em uma única operação.
+
