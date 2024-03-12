4634fd4debe0135229dcc09e22afec760711e417
jonathan vinicius <90495222+jota125@users.noreply.github.com>
2024-03-12 20:04:39 -0300

    Update Lista-1.js

--- Lista-1.js
+++ Lista-1.js
@@ -1,1 +1,16 @@
-/
+//Escreva um programa que solicite ao usuário dois números inteiros e imprima a soma deles.
+var numero1 = parseInt(prompt("Digite o primeiro número inteiro:"));
+var numero2 = parseInt(prompt("Digite o segundo número inteiro:"));
+
+var soma = numero1 + numero2;
+
+console.log("A soma de " + numero1 + " e " + numero2 + " é igual a " + soma + ".");
+
+//Peça ao usuário para inserir três números e calcule a média aritmética desses números.
+var numero1 = prompt("Digite o primeiro número:");
+var numero2 = prompt("Digite o segundo número:");
+var numero3 = prompt("Digite o terceiro número:");
+
+var media = (numero1 + numero2 + numero3) / 3;
+
+console.log("A média dos números " + numero1 + ", " + numero2 + " e " + numero3 + " é: " + media);
